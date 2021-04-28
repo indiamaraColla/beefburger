59d4cfa47d8d706e30f885d6d2e21a57a82cf624
Indiamara Siqueira <indiamara@bsuix.cc>
2021-04-28 16:27:51 -0300

    Criando componestes e deixando o codigo mais limpo.

--- src/pages/FinalizandoFood/index.js
+++ src/pages/FinalizandoFood/index.js
@@ -3,28 +3,17 @@ import { useHistory } from 'react-router-dom';
 import { useBurger } from '../../store/BurgerStore';
 
 import Modal from '../../components/Modal';
-import BtnBlueDark from '../../components/Button/BtnDarkBlue';
 import BtnOutline from '../../components/Button/BtnDarkBlue';
+import BtnBlueDark from '../../components/Button/BtnDarkBlue';
 import Headline from '../../components/Typografy/Headline';
-import Subheading from '../../components/Typografy/Subheading';
 
-import Remover from '../../img/trashCan.png';
-import BagFood from '../../img/bagFood.png';
+import CheckOrder from '../../components/OrderFinish/checkOrder';
+import FinishOrder from '../../components/OrderFinish/finishOrder';
 
-import {
-  Menu,
-  MenuDescription,
-  BoxDescription,
-  BoxList,
-  Box,
-  RemoverLixeira,
-  Adress,
-  Button,
-  TotalPrice,
-} from './style';
+import { Menu, MenuDescription, ContainerModal } from './style';
 
 const FinalizandoFood = () => {
-  const { cardItems, removeCart } = useBurger();
+  const { cardItems, removeCart, setCartItems } = useBurger();
   const [show, showModal] = useState(false);
   const [finalizar, showFinish] = useState(false);
 
@@ -44,87 +33,42 @@ const FinalizandoFood = () => {
     removeCart(id);
   };
 
+  const onFinishOrder = () => {
+    setCartItems('');
+    history.push('/home');
+  };
+
   useEffect(() => {
     if (cardItems.length === 0) {
       history.push('/home');
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cardItems]);
+  }, []);
 
   return (
     <Menu>
       <MenuDescription>
         <BtnOutline onClick={handleClickBack} outline />
 
-        <BoxDescription>
-          <Headline text="seu pedido" primary />
+        <CheckOrder onCloseOk={onCloseOk} cardItems={cardItems} showFinish={showFinish} />
 
-          <div>
-            {cardItems.map((data, index) => (
-              <BoxList key={index}>
-                <div>
-                  <Subheading text={data.title} secondary />
-
-                  <RemoverLixeira>
-                    <span>{data.description}</span>
-                    <div>
-                      <img src={Remover} onClick={() => onCloseOk(data.id)} alt="remover" />
-                    </div>
-                  </RemoverLixeira>
-                </div>
-                <TotalPrice>
-                  <p style={{ color: '#de080d' }}>subtotal</p>
-                  <Subheading text={`R$ ${data.price * data.count},00`} secondary />
-                </TotalPrice>
-              </BoxList>
-            ))}
-          </div>
-
-          <Button className="hidden">
-            <BtnBlueDark onClick={() => showFinish(true)} text="acabei" blueDark />
-          </Button>
-        </BoxDescription>
-
-        {finalizar && (
-          <BoxDescription>
-            <Headline text="finalizando pedido" primary />
-
-            <BoxList>
-              <Box>
-                <div>
-                  <Subheading text="endereço de retirada" secondary />
-                  <Adress>
-                    <img src={BagFood} alt="bagfood" />
-                    <div>
-                      <p>Rua São Cristolvon 950 Centro - Blumenau</p>
-                      <p> (47) 9924-4329</p>
-                      <p> 30-40 minutos</p>
-                    </div>
-                  </Adress>
-                </div>
-              </Box>
-              <TotalPrice>
-                <p style={{ color: '#de080d' }}>total</p>
-                <Subheading text={`R$ ${total},00`} secondary />
-              </TotalPrice>
-            </BoxList>
-            <Button>
-              <BtnBlueDark onClick={handleClickFinish} blueDark text="pronto" />
-            </Button>
-          </BoxDescription>
+        {cardItems.length >= 1 && (
+          <FinishOrder
+            cardItems={cardItems}
+            total={total}
+            handleClickFinish={handleClickFinish}
+            finalizar={finalizar}
+          />
         )}
       </MenuDescription>
 
       <Modal onClose={() => showModal(false)} show={show} height="800px">
-        <div className="modal" id="modal">
-          <Subheading text="acabei" />
-          <div className="content">pedido finalizado com sucesso...</div>
-          <div className="actions">
-            <button className="toggle-button" onClick={onCloseOk}>
-              OK
-            </button>
-          </div>
-        </div>
+        <ContainerModal>
+          <Headline text="acabei" primary />
+
+          <p>seu pedido foi enviado com sucesso...</p>
+          <BtnBlueDark onClick={onFinishOrder} text="OK" blueDark />
+        </ContainerModal>
       </Modal>
     </Menu>
   );
