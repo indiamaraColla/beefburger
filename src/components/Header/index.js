daa4d716f6b54d17c7ea641c2bd81e2226f18da6
Indiamara Siqueira <indiamara@bsuix.cc>
2021-04-28 11:38:51 -0300

    Ajustando header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from './style';
+import { Container, Content } from './style';
 
 import { FaFacebook, FaYoutube } from 'react-icons/fa';
 import { AiFillTwitterCircle } from 'react-icons/ai';
@@ -9,18 +9,20 @@ import TitleLogo from '../TitleLogo';
 function Header() {
   return (
     <Container>
-      <TitleLogo />
-      <div>
-        <a href="https://pt-br.facebook.com/" target="blank">
-          <FaFacebook size={24} style={{ color: '#fff', marginRight: '10' }} />
-        </a>
-        <a href="https://twitter.com/login?lang=pt" target="blank">
-          <AiFillTwitterCircle size={24} style={{ color: '#fff', marginRight: '10' }} />
-        </a>
-        <a href="https://www.youtube.com/?hl=pt&gl=BR" target="blank">
-          <FaYoutube size={24} style={{ color: '#fff' }} />
-        </a>
-      </div>
+      <Content>
+        <TitleLogo />
+        <div>
+          <a href="https://pt-br.facebook.com/" target="blank">
+            <FaFacebook size={24} style={{ color: '#fff', marginRight: '10' }} />
+          </a>
+          <a href="https://twitter.com/login?lang=pt" target="blank">
+            <AiFillTwitterCircle size={24} style={{ color: '#fff', marginRight: '10' }} />
+          </a>
+          <a href="https://www.youtube.com/?hl=pt&gl=BR" target="blank">
+            <FaYoutube size={24} style={{ color: '#fff' }} />
+          </a>
+        </div>
+      </Content>
     </Container>
   );
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -4,7 +4,12 @@ export const Container = styled.div`
   background: #d41515;
   width: 100%;
   padding: 16px;
+`;
+
+export const Content = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  max-width: 820px;
+  margin: 0 auto;
 `;
