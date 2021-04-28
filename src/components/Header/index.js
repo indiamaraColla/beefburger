a94e7a9ab537f3102f9c7068985365028d00a679
Indiamara Siqueira <indiamara@bsuix.cc>
2021-04-28 11:09:45 -0300

    Removendo e corrigindo codigo não usados ou incorretos

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MainHeader, Container, List } from './style';
+import { Container } from './style';
 
 import { FaFacebook, FaYoutube } from 'react-icons/fa';
 import { AiFillTwitterCircle } from 'react-icons/ai';
@@ -8,22 +8,20 @@ import TitleLogo from '../TitleLogo';
 
 function Header() {
   return (
-    <MainHeader>
-      <Container>
-        <TitleLogo />
-        <List>
-          <a href="https://pt-br.facebook.com/" target="blank">
-            <FaFacebook size={24} color="#fff" />
-          </a>
-          <a href="https://twitter.com/login?lang=pt" target="blank">
-            <AiFillTwitterCircle size={24} color="#fff" />
-          </a>
-          <a href="https://www.youtube.com/?hl=pt&gl=BR" target="blank">
-            <FaYoutube size={24} color="#fff" />
-          </a>
-        </List>
-      </Container>
-    </MainHeader>
+    <Container>
+      <TitleLogo />
+      <div>
+        <a href="https://pt-br.facebook.com/" target="blank">
+          <FaFacebook size={24} style={{ color: '#fff', marginRight: '10' }} />
+        </a>
+        <a href="https://twitter.com/login?lang=pt" target="blank">
+          <AiFillTwitterCircle size={24} style={{ color: '#fff', marginRight: '10' }} />
+        </a>
+        <a href="https://www.youtube.com/?hl=pt&gl=BR" target="blank">
+          <FaYoutube size={24} style={{ color: '#fff' }} />
+        </a>
+      </div>
+    </Container>
   );
 }
 export default Header;
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,19 +1,10 @@
 import styled from 'styled-components';
 
-export const MainHeader = styled.div`
-  background: #d41515;
-`;
 export const Container = styled.div`
+  background: #d41515;
   width: 100%;
   padding: 16px;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  max-width: 820px;
-  margin: 0 auto;
-`;
-export const List = styled.ul`
-  svg {
-    margin-right: 10px;
-  }
 `;
