04fc0cbcea492b99961c379ed7ae5b298c696128
Indiamara Siqueira <indiamara@bsuix.cc>
2021-04-28 11:11:57 -0300

    Removendo espaço em branco

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -1,13 +1,11 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const TextField = styled.input`
   margin: 8px 0;
   padding: 8px;
   border-radius: 8px;
-  border: 2px solid #D41515;
+  border: 2px solid #d41515;
   font-size: 14px;
   font-weight: bold;
   width: 100%;
-
-  
 `;
